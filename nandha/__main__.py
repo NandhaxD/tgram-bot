cc37e2eb16031dc59145c8d234201a4d3cf80730
Nandhagopal <89440790+NandhaxD@users.noreply.github.com>
2025-10-05 13:14:49 -0300

    Update __main__.py

--- nandha/__main__.py
+++ nandha/__main__.py
@@ -1,1 +1,16 @@
+import asyncio
 
+from nandha import bot
+from info import ExtConfig
+
+async def main():
+    await bot.run_for_updates()
+    await bot.send_message(
+         chat_id=ExtConfig.support,
+         text="Bot Re-Started!"
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
