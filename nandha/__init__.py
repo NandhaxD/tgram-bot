5ebc58ee73302be17346b27b8c3fc9d7ee129e7a
Nandhagopal <89440790+NandhaxD@users.noreply.github.com>
2025-10-05 13:03:52 -0300

    Update __init__.py

--- nandha/__init__.py
+++ nandha/__init__.py
@@ -1,1 +1,16 @@
 
+import logging
+
+
+from info import BotConfig, ExtConfig
+from tgram import TgBot
+
+FORMAT = f"[Bot] %(message)s"
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'), logging.StreamHandler()], format=FORMAT)
+logging.getLogger('httpx').setLevel(logging.WARNING)
+
+bot = TgBot(
+  bot_token=BotConfig.token,
+  parse_mode=ExtConfig.parse_mode,
+  plugins="nandha/plugins"
+)
